5b8fe470f0a6bcc93a652282966eebe7e1bbb763
Marcin Marczyk <m.l.marczyk@gmail.com>
2015-08-06 00:44:13 +0200

    added hobbys

--- script/core/hobby/hobbyCtrl.js
+++ script/core/hobby/hobbyCtrl.js
@@ -1,4 +1,15 @@
 app.controller('HobbyCtrl', function($scope, $routeParams){
     $scope.name = "HobbyCtrl";
     $scope.params = $routeParams;
+    $scope.schools = [
+        {
+            name: 'Home beer brewing'
+        },
+        {
+            name: 'Home automation using Raspberry Pi, Arduino'
+        },
+        {
+        	name: 'New technologies'
+        }
+    ];
 });
